89f82bc0560e4688e67b98a1f51112c184094076
akihyro <akihyro@users.noreply.github.com>
2014-11-16 19:17:48 +0900

    レスポンスのコンテンツタイプをURL拡張子で切り替える

--- src/main/java/tryrest/TryRestApplication.java
+++ src/main/java/tryrest/TryRestApplication.java
@@ -1,6 +1,12 @@
 package tryrest;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.ws.rs.core.MediaType;
+
 import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.server.ServerProperties;
 
 /**
  * アプリケーション。
@@ -12,6 +18,12 @@ public class TryRestApplication extends ResourceConfig {
      */
     public TryRestApplication() {
         packages(TryRestApplication.class.getPackage().getName());
+
+        // URL拡張子とコンテンツタイプのマッピング
+        Map<String, MediaType> mappings = new HashMap<>();
+        mappings.put("xml", MediaType.APPLICATION_XML_TYPE);
+        mappings.put("json", MediaType.APPLICATION_JSON_TYPE);
+        property(ServerProperties.MEDIA_TYPE_MAPPINGS, mappings);
     }
 
 }
